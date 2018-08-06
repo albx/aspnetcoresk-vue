7b5d320db54a5f93f9485677dfd6bc362546bcc9
Alberto Mori <albi.mo87@hotmail.it>
2018-08-06 17:43:22 +0200

    first setup

---
+++ AspNetCoreSK.Vue/webpack.config.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const distPath = path.join(__dirname, 'wwwroot');
+
+module.exports = {
+    entry: {
+        'jquery': [
+            'jquery/dist/jquery.js',
+            'jquery/dist/jquery.min.js'
+        ],
+        'bootstrap': [
+            'bootstrap/dist/js/bootstrap.js',
+            'bootstrap/dist/js/bootstrap.min.js',
+            'bootstrap/dist/css/bootstrap.css',
+            'bootstrap/dist/css/bootstrap.min.css'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, './wwwroot/dist'),
+        publicPath: '/',
+        filename: 'js/[name].js'
+    },
+    resolve: {
+        modules: ['./node_modules']
+    },
+    plugins: [
+        new ExtractTextPlugin('css/[name].css')
+    ]
+};
